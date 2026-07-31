f4552afc1580fa5944235c729f775e04ab0ab3fa
Aswin Rejoe <aswin1308@gmail.com>
2026-07-31 10:39:39 +0530

    Initial Green Hub project

---
+++ server/server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const cors = require("cors");
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+const PORT = process.env.PORT || 5000;
+
+app.get("/", (req, res) => {
+  res.send("Green Hub API is running...");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
